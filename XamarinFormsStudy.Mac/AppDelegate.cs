df5b68dd4ae7fbddef0d8051ddff7bf3341302fb
imaizm <imaizm@gmail.com>
2020-07-14 22:33:08 +0900

    implements ApplicationShouldTerminateAfterLastWindowClosed

--- XamarinFormsStudy.Mac/AppDelegate.cs
+++ XamarinFormsStudy.Mac/AppDelegate.cs
@@ -40,5 +40,10 @@ namespace XamarinFormsStudy.Mac
         {
             // Insert code here to tear down your application
         }
+
+        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
+        {
+            return true;
+        }
     }
 }
