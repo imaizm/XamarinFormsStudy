36e5be8ec7269362b4dec84496c97a04cdeab74c
imaizm <imaizm@gmail.com>
2020-07-20 10:52:00 +0900

    Behavior仮追加

---
+++ XamarinFormsStudy/Behaviors/ItemSelectedToCommandBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinFormsStudy.Behaviors
+{
+    public class ItemSelectedToCommandBehavior : Behavior<ListView>
+    {
+        public ItemSelectedToCommandBehavior()
+        {
+        }
+
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.ItemSelected += (s, e) =>
+            {
+                if (e.SelectedItem != null)
+                {
+                    
+                }
+            };
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
