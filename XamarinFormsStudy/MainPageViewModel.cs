aec8f9d6e8326e0ad664910cf51cc32932592305
imaizm <imaizm@gmail.com>
2020-07-20 10:51:39 +0900

    add FileInfoList

--- XamarinFormsStudy/MainPageViewModel.cs
+++ XamarinFormsStudy/MainPageViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Input;
 using XamarinFormsStudy.Views;
 using XamarinFormsStudy.Commands;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
 
 namespace XamarinFormsStudy
 {
@@ -29,6 +34,8 @@ namespace XamarinFormsStudy
             }
         }
 
+        public ObservableCollection<FileInfo> FileInfoList { get; set; }
+
         public ICommand UpdateBodyMessageCommand { get; }
         public ICommand OpenFilePickerCommand { get; }
 
@@ -36,6 +43,47 @@ namespace XamarinFormsStudy
         {
             this.UpdateBodyMessageCommand = new UpdateBodyMessage(this);
             this.OpenFilePickerCommand = new OpenFilePickerCommand(this);
+
+            var path = Environment.GetFolderPath(
+                Environment.SpecialFolder.UserProfile);
+
+            var files = Directory.EnumerateFileSystemEntries(path).OrderBy(value => value);
+            
+
+            this.FileInfoList = new ObservableCollection<FileInfo>();
+
+            var parentPath = new FileInfo();
+            parentPath.IsFile = false;
+            parentPath.IsSelected = false;
+            parentPath.IsSelectable = false;
+            parentPath.Title = "..";
+            parentPath.Path = Directory.GetParent(path).ToString();
+            parentPath.BackGroundColor = Color.LightYellow;
+            this.FileInfoList.Add(parentPath);
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo();
+                fileInfo.IsFile = !
+                    File.GetAttributes(file).HasFlag(FileAttributes.Directory);
+                fileInfo.IsSelected = false;
+                fileInfo.IsSelectable = true;
+                fileInfo.Title = file.Replace(path + Path.DirectorySeparatorChar, "");
+                fileInfo.Path = file;
+                fileInfo.BackGroundColor = (fileInfo.IsFile) ? Color.White : Color.LightYellow;
+                this.FileInfoList.Add(fileInfo);
+            }
+
         }
+    }
+
+    public class FileInfo
+    {
+        public bool IsFile { get; set; }
+        public bool IsSelected { get; set; }
+        public bool IsSelectable { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public Color BackGroundColor { get; set; }
     }
 }
